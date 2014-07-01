3c766f7726895ef0d3f342d8f3ba725dc08442c8
lukascivil <lukas_civil@hotmail.com>
2014-07-01 00:48:58 -0300

    change(BrickBreak to BrickBreakMain , classname and import)

--- desktop/src/com/brickbreak/game/desktop/DesktopLauncher.java
+++ desktop/src/com/brickbreak/game/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.brickbreak.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.brickbreak.game.BrickBreak;
+import com.brickbreak.game.BrickBreakMain;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new BrickBreak(), config);
+		new LwjglApplication(new BrickBreakMain(), config);
 	}
 }
--- ios/src/com/brickbreak/game/IOSLauncher.java
+++ ios/src/com/brickbreak/game/IOSLauncher.java
@@ -5,13 +5,13 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.brickbreak.game.BrickBreak;
+import com.brickbreak.game.BrickBreakMain;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new BrickBreak(), config);
+        return new IOSApplication(new BrickBreakMain(), config);
     }
 
     public static void main(String[] argv) {

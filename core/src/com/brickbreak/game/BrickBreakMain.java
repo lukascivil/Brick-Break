fe201f5743d215be35d6705c42ac95004fcdf1b9
lukascivil <lukas_civil@hotmail.com>
2014-07-01 00:41:33 -0300

    BrickBreakMain was created

---
+++ core/src/com/brickbreak/game/BrickBreakMain.java
@@ -0,0 +1,38 @@
+package com.brickbreak.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class BrickBreakMain extends ApplicationAdapter
+{
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create ()
+	{
+		//initialize controller and renderer
+		worldController = new WorldController();
+		worldRenderer = new WorldRenderer(worldController);
+	}
+
+	@Override
+	public void render ()//LOOP , LOOP , LOOP , LOOP , LOOP, LOOP 
+	{
+		worldRenderer.render();
+		//O que pode acontecer com os objetos durante o jogo;
+		worldController.update();
+		
+	}
+	public void resize(int width, int height)
+	{
+		worldRenderer.resize(width, height);
+	}
+	public void dispose()
+	{
+		worldRenderer.dispose();	
+	}
+}

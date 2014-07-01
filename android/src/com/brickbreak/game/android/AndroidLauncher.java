3e3594fcc06d71359b416ede127761e0d9f12414
lukascivil <lukas_civil@hotmail.com>
2014-07-01 00:50:37 -0300

    change(BrickBreak to BrickBreakMain , classname and import)

--- android/src/com/brickbreak/game/android/AndroidLauncher.java
+++ android/src/com/brickbreak/game/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.brickbreak.game.BrickBreak;
+import com.brickbreak.game.BrickBreakMain;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new BrickBreak(), config);
+		initialize(new BrickBreakMain(), config);
 	}
 }
